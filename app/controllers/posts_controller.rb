596fab3b89441bdc1b55744f8c4b9ffb7c269b0d
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2022-12-18 01:30:17 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,11 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.order(id: "DESC")
   end
 
-  #def new
-  #end
-
   def create
     Post.create(content: params[:content])
-    redirect_to action: :index
+    render json:{post: post }
+    #7行目で定義した変数postの値をpostというキーとセットでJSに送信している。JSでは受け取った変数postを使用しメモをブラウザに反映させる。
   end
 end
